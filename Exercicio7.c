78243b4c8243e145741316579006393c5637e23b
Caiot28 <141367836+Caiot28@users.noreply.github.com>
2023-09-19 11:27:47 -0300

    Add files via upload

---
+++ Exercicio7.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+main(){
+
+    char sexo [2];
+    char corOlhos [2];
+    char corCabelo [2];
+    int idade;
+    float salario;
+
+    do
+    {
+       
+    } while (idade != -1);
+    
+
+
+
+
+}
